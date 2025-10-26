7bf39a2037099f2916a97977216a0a4f22bd0abf
Alex <nilzon.gomez02@gmail.com>
2025-10-25 23:01:31 -0500

    Nodos y Listas dobles

---
+++ Nodos-Listas-Dobles/DoubleNode.py
@@ -0,0 +1,30 @@
+class DoubleNode:
+    def __init__(self, prev=None, next=None, data=None):
+        self.__data = data
+        self.__next = next
+        self.__prev = prev
+
+    @property
+    def data(self):
+        return self.__data
+    
+    @property
+    def next(self):
+        return self.__next
+    
+    @property
+    def prev(self):
+        return self.__prev
+    
+    @data.setter
+    def data(self, d):
+        self.__data = d
+
+    @next.setter
+    def next(self, n):
+        self.__next = n
+
+    @prev.setter
+    def prev(self, p):
+        self.__prev = p
+
